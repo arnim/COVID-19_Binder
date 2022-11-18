0ff86bbc57b7b5c51f87b8f065b2b7bb2e9f2ec5
Arnim Bleier <arnim.bleier@gmail.com>
2022-11-18 03:59:35 +0100

    rm ggthemes

--- myLib.R
+++ myLib.R
@@ -7,7 +7,6 @@ suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(readr))
 suppressPackageStartupMessages(library(scales))
-suppressPackageStartupMessages(library(ggthemes))
 
 options(repr.plot.width=12, repr.plot.height=5)
 theme_set(theme_minimal(base_size = 16))
